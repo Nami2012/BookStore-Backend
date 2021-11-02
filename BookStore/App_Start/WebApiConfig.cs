9443b960eec141be065d2624363c55745f3403af
Nami2012 <namithasadeeshbabu@gmail.com>
2021-11-02 20:03:41 +0530

    Enable cors

--- BookStore/App_Start/WebApiConfig.cs
+++ BookStore/App_Start/WebApiConfig.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-
+using System.Web.Http.Cors;
 
 namespace BookStore
 {
@@ -11,7 +11,8 @@ namespace BookStore
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            EnableCorsAttribute cors = new EnableCorsAttribute("http://localhost:3000/SampleApp/Form1.aspx", "*", "GET,POST"); 
+            config.EnableCors(cors);
             // Web API routes
             config.MapHttpAttributeRoutes();
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # BookStore-Backend
+
+
+# dependencies
+Entity Framework
+Microsoft.AspNet.WebApi.Cors => 2.2.0
