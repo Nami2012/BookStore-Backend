20845a570fe1d6d220863a441e21e089d830da3d
Nami2012 <namithasadeeshbabu@gmail.com>
2021-11-05 00:08:27 +0530

    Merge with the contents of UserandCart branch

---
+++ BookStore/Controllers/User_Account_InfoController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class User_Account_InfoController : ApiController
+    {
+        private BookStoreDBEntities db = new BookStoreDBEntities();
+
+        // GET: api/User_Account_Info
+        public IQueryable<User_Account_Info> GetUser_Account_Info()
+        {
+            return db.User_Account_Info;
+        }
+
+        // GET: api/User_Account_Info/5
+        [ResponseType(typeof(User_Account_Info))]
+        public IHttpActionResult GetUser_Account_Info(int id)
+        {
+            User_Account_Info user_Account_Info = db.User_Account_Info.Find(id);
+            if (user_Account_Info == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user_Account_Info);
+        }
+
+        // GET: api/user/{UserId}/activestatus
+        [HttpGet]
+        [Route("api/user/{id}/activestatus")]
+        public IHttpActionResult GetUserActiveStatus(int id)
+        {
+            User_Account_Info user = db.User_Account_Info.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user.ActiveStatus);
+        }
+
+        // PUT: api/user/{id}/activestatus
+        [HttpPut]
+        [Route("api/user/{id}/activestatus")]
+        public IHttpActionResult PutUserActiveStatus(int id, bool newStatus)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            User_Account_Info user = db.User_Account_Info.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.ActiveStatus = newStatus;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+                if (!User_Account_InfoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // PUT: api/User_Account_Info/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUser_Account_Info(int id, User_Account_Info user_Account_Info)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user_Account_Info.UId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user_Account_Info).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!User_Account_InfoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/User_Account_Info
+        [ResponseType(typeof(User_Account_Info))]
+        public IHttpActionResult PostUser_Account_Info(User_Account_Info user_Account_Info)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.User_Account_Info.Add(user_Account_Info);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (User_Account_InfoExists(user_Account_Info.UId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = user_Account_Info.UId }, user_Account_Info);
+        }
+
+        // DELETE: api/User_Account_Info/5
+        [ResponseType(typeof(User_Account_Info))]
+        public IHttpActionResult DeleteUser_Account_Info(int id)
+        {
+            User_Account_Info user_Account_Info = db.User_Account_Info.Find(id);
+            if (user_Account_Info == null)
+            {
+                return NotFound();
+            }
+
+            db.User_Account_Info.Remove(user_Account_Info);
+            db.SaveChanges();
+
+            return Ok(user_Account_Info);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool User_Account_InfoExists(int id)
+        {
+            return db.User_Account_Info.Count(e => e.UId == id) > 0;
+        }
+    }
+}
